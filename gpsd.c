1f6591779df0edf1da0ad9e6c148b2f28fd83c6f
GUilherme <guimdo1999@hotmail.com>
2023-05-29 12:16:46 -0300

    Leitura

--- gpsd.c
+++ gpsd.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <gps.h>
+#include <unistd.h>
+
+#define GPSD_BUFFER_SIZE 512
 
 int main() {
     struct gps_data_t gpsdata;
+    char gpsd_message[GPSD_BUFFER_SIZE];
     
     // Inicializa a conexão com o GPS
     if (gps_open("localhost", "2947", &gpsdata) < 0) {
@@ -18,16 +22,23 @@ int main() {
         // Aguarda os dados do GPS
         if (gps_waiting(&gpsdata, 5000000)) {
             // Lê os dados do GPS
-            if (gps_read(&gpsdata, NULL, 0) == -1) {
+            if (gps_read(&gpsdata, gpsd_message, sizeof(gpsd_message)) == -1) {
                 fprintf(stderr, "Falha ao ler dados do GPS.\n");
                 break;
             }
-            
             printf("Socket selecionado corretamente.\n");
-            break;
+            
+            // Imprime os dados de latitude e longitude
+            if (gpsdata.fix.mode >= MODE_2D) {
+                printf("Latitude: %.6f\n", gpsdata.fix.latitude);
+                printf("Longitude: %.6f\n", gpsdata.fix.longitude);
+            }
         } else {
             fprintf(stderr, "Socket não está pronto, aguardando...\n");
         }
+        
+        // Aguarda 1 segundo
+        sleep(1);
     }
     
     // Fecha a conexão com o GPS
