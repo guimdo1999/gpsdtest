2441c1036c3caa829e925e7156aa174e918985ea
GUilherme <guimdo1999@hotmail.com>
2023-06-02 09:44:17 -0300

    Scan kill program

--- scan.c
+++ scan.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <signal.h>
+
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
 #define UUID_SIZE 16
-#define UUID_FILTER { 0xFA, 0xFF }
 
-int main() {
+static bool kill_program = false;
+
+static void sig_handler(int signo)
+{
+    if (signo == SIGINT || signo == SIGSTOP || signo == SIGKILL || signo == SIGTERM)
+    {
+        kill_program = true;
+    }
+}
+
+int main()
+{
     int device_id = hci_get_route(NULL);
     int socket = hci_open_dev(device_id);
-    if (socket < 0) {
+    if (socket < 0)
+    {
         perror("Failed to open HCI socket");
         exit(1);
     }
@@ -22,21 +35,33 @@ int main() {
     hci_filter_set_ptype(HCI_EVENT_PKT, &filter);
     hci_filter_set_event(EVT_LE_META_EVENT, &filter);
 
-    if (setsockopt(socket, SOL_HCI, HCI_FILTER, &filter, sizeof(filter)) < 0) {
+    signal(SIGINT, sig_handler);
+    signal(SIGKILL, sig_handler);
+    signal(SIGSTOP, sig_handler);
+    signal(SIGTERM, sig_handler);
+
+    if (setsockopt(socket, SOL_HCI, HCI_FILTER, &filter, sizeof(filter)) < 0)
+    {
         perror("Failed to set socket options");
         close(socket);
         exit(1);
     }
 
     // Loop para receber e tratar os eventos
-    while (1) {
+    while (1)
+    {
+        if (kill_program)
+                break;
         unsigned char buf[HCI_MAX_EVENT_SIZE];
         int len = read(socket, buf, sizeof(buf));
-        if (len >= HCI_EVENT_HDR_SIZE) {
-            evt_le_meta_event* meta_event = (evt_le_meta_event*)(buf + HCI_EVENT_HDR_SIZE + 1);
-            if (meta_event->subevent == EVT_LE_ADVERTISING_REPORT) {
-                le_advertising_info* info = (le_advertising_info*)(meta_event->data + 1);
-                if (info->length >= 18 && info->data[13] == 0xFA && info->data[14] == 0xFF) {
+        if (len >= HCI_EVENT_HDR_SIZE)
+        {
+            evt_le_meta_event *meta_event = (evt_le_meta_event *)(buf + HCI_EVENT_HDR_SIZE + 1);
+            if (meta_event->subevent == EVT_LE_ADVERTISING_REPORT)
+            {
+                le_advertising_info *info = (le_advertising_info *)(meta_event->data + 1);
+                if (info->length >= 18 && info->data[13] == 0xFA && info->data[14] == 0xFF)
+                {
                     // Processa os dados do dispositivo com o UUID 0xFFFA
                     printf("Dispositivo encontrado com UUID 0xFFFA:\n");
                     char addr[18];
