c91a811e696afaabbfbc1bfd0f23710105d41852
GUilherme <guimdo1999@hotmail.com>
2023-06-02 09:36:39 -0300

    Scan

--- scan.c
+++ scan.c
@@ -1,145 +1,54 @@
-//
-//  Intel Edison Playground
-//  Copyright (c) 2015 Damian Kołakowski. All rights reserved.
-//
-
+#include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <curses.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
-struct hci_request ble_hci_request(uint16_t ocf, int clen, void * status, void * cparam)
-{
-	struct hci_request rq;
-	memset(&rq, 0, sizeof(rq));
-	rq.ogf = OGF_LE_CTL;
-	rq.ocf = ocf;
-	rq.cparam = cparam;
-	rq.clen = clen;
-	rq.rparam = status;
-	rq.rlen = 1;
-	return rq;
-}
+#define UUID_SIZE 16
+#define UUID_FILTER { 0xFA, 0xFF }
 
-int main()
-{
-	int ret, status;
+int main() {
+    int device_id = hci_get_route(NULL);
+    int socket = hci_open_dev(device_id);
+    if (socket < 0) {
+        perror("Failed to open HCI socket");
+        exit(1);
+    }
 
-	// Get HCI device.
+    // Configura o filtro para capturar apenas eventos de anúncio
+    struct hci_filter filter;
+    hci_filter_clear(&filter);
+    hci_filter_set_ptype(HCI_EVENT_PKT, &filter);
+    hci_filter_set_event(EVT_LE_META_EVENT, &filter);
 
-	const int device = hci_open_dev(hci_get_route(NULL));
-	if ( device < 0 ) { 
-		perror("Failed to open HCI device.");
-		return 0; 
-	}
-		hci_send_cmd(device, OGF_HOST_CTL, OCF_RESET, 0, 0);
+    if (setsockopt(socket, SOL_HCI, HCI_FILTER, &filter, sizeof(filter)) < 0) {
+        perror("Failed to set socket options");
+        close(socket);
+        exit(1);
+    }
 
-	// Set BLE scan parameters.
-	
-	le_set_scan_parameters_cp scan_params_cp;
-	memset(&scan_params_cp, 0, sizeof(scan_params_cp));
-	scan_params_cp.type 			= 0x00; 
-	scan_params_cp.interval 		= htobs(0x0010);
-	scan_params_cp.window 			= htobs(0x0010);
-	scan_params_cp.own_bdaddr_type 	= 0x00; // Public Device Address (default).
-	scan_params_cp.filter 			= 0x00; // Accept all.
+    // Loop para receber e tratar os eventos
+    while (1) {
+        unsigned char buf[HCI_MAX_EVENT_SIZE];
+        int len = read(socket, buf, sizeof(buf));
+        if (len >= HCI_EVENT_HDR_SIZE) {
+            evt_le_meta_event* meta_event = (evt_le_meta_event*)(buf + HCI_EVENT_HDR_SIZE + 1);
+            if (meta_event->subevent == EVT_LE_ADVERTISING_REPORT) {
+                le_advertising_info* info = (le_advertising_info*)(meta_event->data + 1);
+                if (info->length >= 18 && info->data[13] == 0xFA && info->data[14] == 0xFF) {
+                    // Processa os dados do dispositivo com o UUID 0xFFFA
+                    printf("Dispositivo encontrado com UUID 0xFFFA:\n");
+                    char addr[18];
+                    ba2str(&info->bdaddr, addr);
+                    printf("Endereço: %s\n", addr);
+                    // Outros dados do dispositivo
+                    // ...
+                }
+            }
+        }
+    }
 
-	struct hci_request scan_params_rq = ble_hci_request(OCF_LE_SET_SCAN_PARAMETERS, LE_SET_SCAN_PARAMETERS_CP_SIZE, &status, &scan_params_cp);
-	
-	ret = hci_send_req(device, &scan_params_rq, 1000);
-	if ( ret < 0 ) {
-		hci_close_dev(device);
-		perror("Failed to set scan parameters data.");
-		return 0;
-	}
-
-	// Set BLE events report mask.
-
-	le_set_event_mask_cp event_mask_cp;
-	memset(&event_mask_cp, 0, sizeof(le_set_event_mask_cp));
-	int i = 0;
-	for ( i = 0 ; i < 8 ; i++ ) event_mask_cp.mask[i] = 0xFF;
-
-	struct hci_request set_mask_rq = ble_hci_request(OCF_LE_SET_EVENT_MASK, LE_SET_EVENT_MASK_CP_SIZE, &status, &event_mask_cp);
-	ret = hci_send_req(device, &set_mask_rq, 1000);
-	if ( ret < 0 ) {
-		hci_close_dev(device);
-		perror("Failed to set event mask.");
-		return 0;
-	}
-
-	// Enable scanning.
-
-	le_set_scan_enable_cp scan_cp;
-	memset(&scan_cp, 0, sizeof(scan_cp));
-	scan_cp.enable 		= 0x01;	// Enable flag.
-	scan_cp.filter_dup 	= 0x00; // Filtering disabled.
-
-	struct hci_request enable_adv_rq = ble_hci_request(OCF_LE_SET_SCAN_ENABLE, LE_SET_SCAN_ENABLE_CP_SIZE, &status, &scan_cp);
-
-	ret = hci_send_req(device, &enable_adv_rq, 1000);
-	if ( ret < 0 ) {
-		hci_close_dev(device);
-		perror("Failed to enable scan.");
-		return 0;
-	}
-
-	// Get Results.
-
-	struct hci_filter nf;
-	hci_filter_clear(&nf);
-	hci_filter_set_ptype(HCI_EVENT_PKT, &nf);
-	hci_filter_set_event(EVT_LE_META_EVENT, &nf);
-	if ( setsockopt(device, SOL_HCI, HCI_FILTER, &nf, sizeof(nf)) < 0 ) {
-		hci_close_dev(device);
-		perror("Could not set socket options\n");
-		return 0;
-	}
-
-	printf("Scanning....\n");
-
-	uint8_t buf[HCI_MAX_EVENT_SIZE];
-	evt_le_meta_event * meta_event;
-	le_advertising_info * info;
-	int len;
-
-	while ( 1 ) {
-		len = read(device, buf, sizeof(buf));
-		if ( len >= HCI_EVENT_HDR_SIZE ) {
-			meta_event = (evt_le_meta_event*)(buf+HCI_EVENT_HDR_SIZE+1);
-			if ( meta_event->subevent == EVT_LE_ADVERTISING_REPORT ) {
-				uint8_t reports_count = meta_event->data[0];
-				void * offset = meta_event->data + 1;
-				while ( reports_count-- ) {
-					info = (le_advertising_info *)offset;
-					char addr[18];
-					ba2str(&(info->bdaddr), addr);
-					printf("%s - RSSI %d\n", addr, (char)info->data[info->length]);
-					offset = info->data + info->length + 2;
-				}
-			}
-		}
-	}
-
-	// Disable scanning.
-
-	memset(&scan_cp, 0, sizeof(scan_cp));
-	scan_cp.enable = 0x00;	// Disable flag.
-
-	struct hci_request disable_adv_rq = ble_hci_request(OCF_LE_SET_SCAN_ENABLE, LE_SET_SCAN_ENABLE_CP_SIZE, &status, &scan_cp);
-	ret = hci_send_req(device, &disable_adv_rq, 1000);
-	if ( ret < 0 ) {
-		hci_close_dev(device);
-		perror("Failed to disable scan.");
-		return 0;
-	}
-
-	hci_close_dev(device);
-	
-	return 0;
+    close(socket);
+    return 0;
 }
